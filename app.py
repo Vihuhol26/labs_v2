3ff80cd47c0b2ef3fe7e2eda9795bd75dcd05d05
Chuvashova Margarita <ritachuv88@gmail.com>
2024-10-03 15:12:08 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,5 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
-    
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Чувашова Маргарита Вячеславовна, лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy; Чувашова Маргарита, ФБИ-23, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
