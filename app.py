55ffc3e5c8e4a9633951d3b1e229fc9ad40d9097
Chuvashova Margarita <ritachuv88@gmail.com>
2024-10-18 20:18:42 +0700

    cry baby

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def menu():
 <!doctype html>
 <html>
     <head>
-        <link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+        <link rel="stylesheet" href="''' + url_for('static', filename='main.css') + '''">
         <title>НГТУ, ФБ, Лабораторные работы</title>
     </head>
     <body>
@@ -30,6 +30,7 @@ def menu():
 
         <ol>
             <li><a href="http://127.0.0.1:5000/lab1">Первая лабораторная</a></li>
+            <li><a href="http://127.0.0.1:5000/lab2">Вторая лабораторная</a></li>
             <li><a href="http://127.0.0.1:5000/lab2">Вторая лабораторная</a></li>
         </ol>
 
