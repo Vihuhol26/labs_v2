c68bbe08a8a53c604b5f65760b409c7864abd500
Vihuhol26 <ritachuv88@gmail.com>
2024-11-30 00:57:11 +0700

    Заказ зерна

--- lab4.py
+++ lab4.py
@@ -189,3 +189,66 @@ def fridge():
     else:
         error = "Установлена температура: " + str(temp) + "°С ❆"
         return render_template("lab4/temp.html", error = error)
+
+@lab4.route("/lab4/offer/", methods=["POST", "GET"])
+def offer():
+    type_offer = request.form.get("type_offer")
+    if request.method == "GET":
+        return render_template("lab4/offer.html")
+    
+    weight = request.form.get("weight")
+    if not weight:
+        error = "Не введён вес!"
+        return render_template("lab4/offer.html", error = error)
+    elif int(weight) <= 0:
+        error = "Неверное значение веса!"
+        return render_template("lab4/offer.html", error = error)
+    elif int(weight) > 500:
+        error = "Такого объёма сейчас нет в наличии!"
+        return render_template("lab4/offer.html", error = error)
+    elif str(type_offer) == "zerno":
+        error = "Выберите зерно!"
+        return render_template("lab4/offer.html", error = error)
+    elif int(weight) <= 500 and int(weight) >= 50:
+        weight = int(weight)
+        prices = {
+        'yach': 12000,
+        'oves': 8500,
+        'psh': 8700,
+        'poz': 14000
+        }
+        price_per_ton = prices[type_offer]
+        total_price = weight * price_per_ton
+        total_price *= 0.9
+
+        if type_offer == "yach":
+            type_offer = "ячмень"
+        elif type_offer == "psh":
+            type_offer = "пшеница"
+        elif type_offer == "poz":
+            type_offer = "рожь"
+        else:
+            type_offer = "овес"
+
+        return render_template("lab4/offer_conf%.html", total_price = total_price, type_offer = type_offer, weight = weight)
+    else:
+        weight = int(weight)
+        prices = {
+        'yach': 12000,
+        'oves': 8500,
+        'psh': 8700,
+        'poz': 14000
+        }
+        price_per_ton = prices[type_offer]
+        total_price = weight * price_per_ton
+
+        if type_offer == "yach":
+            type_offer = "ячмень"
+        elif type_offer == "psh":
+            type_offer = "пшеница"
+        elif type_offer == "poz":
+            type_offer = "рожь"
+        else:
+            type_offer = "овес"
+
+        return render_template("lab4/offer_conf.html", total_price = total_price, weight = weight, type_offer = type_offer)
