465aa227dff51f4774e7bfb74c16ab249608c64f
Vihuhol26 <ritachuv88@gmail.com>
2024-11-29 23:03:01 +0700

    Счетчик деревьев

--- lab4.py
+++ lab4.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 lab4 = Blueprint('lab4', __name__)
 
 @lab4.route('/lab4/')
@@ -19,3 +19,61 @@ def div():
     x2 = int(x2)
     result = x1 / x2
     return render_template('lab4/div.html', x1=x1, x2=x2, result=result)
+
+@lab4.route("/lab4/add", methods=['POST'])
+def add():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    x1 = int(x1) if x1 else 0
+    x2 = int(x2) if x2 else 0
+    result = x1 + x2
+    return render_template('lab4/div.html', operation="суммирования", x1=x1, x2=x2, result=result)
+
+@lab4.route("/lab4/multiply", methods=['POST'])
+def multiply():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    x1 = int(x1) if x1 else 1
+    x2 = int(x2) if x2 else 1
+    result = x1 * x2
+    return render_template('lab4/div.html', operation="умножения", x1=x1, x2=x2, result=result)
+
+@lab4.route("/lab4/subtract", methods=['POST'])
+def subtract():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '' or x2 == '':
+        return render_template('lab4/div.html', error="Оба поля должны быть заполнены для вычитания")
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1 - x2
+    return render_template('lab4/div.html', operation="вычитания", x1=x1, x2=x2, result=result)
+
+@lab4.route("/lab4/power", methods=['POST'])
+def power():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '' or x2 == '':
+        return render_template('lab4/div.html', error="Оба поля должны быть заполнены для возведения в степень")
+    x1 = int(x1)
+    x2 = int(x2)
+    if x1 == 0 and x2 == 0:
+        return render_template('lab4/div.html', error="Оба значения не могут быть равны нулю для возведения в степень")
+    result = x1 ** x2
+    return render_template('lab4/div.html', operation="возведения в степень", x1=x1, x2=x2, result=result)
+
+tree_count = 0  
+@lab4.route("/lab4/tree", methods=["GET", "POST"])
+def tree():
+    global tree_count
+    if request.method == "POST":
+        operation = request.form.get("operation")
+        if operation == "plant":
+            tree_count += 1
+        elif operation == "cut" and tree_count > 0:
+            tree_count -= 1
+        return redirect (url_for("lab4.tree")) 
+    return render_template("lab4/tree.html", tree_count=tree_count)
+
+if __name__ == "__main__":
+    lab4.run(debug=True)
