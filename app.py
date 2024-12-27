bee175b0eca7049f9b871f1ed642c51d4331d4ac
Chuvashova Margarita <ritachuv88@gmail.com>
2024-12-27 15:01:25 +0700

    «Заготовка к 7-ой лабораторной

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
+from lab7 import lab7
 from rgz import rgz
 import secrets
 
@@ -21,6 +22,7 @@ app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
+app.register_blueprint(lab7)
 app.register_blueprint(rgz)
 
 app.config['JSON_AS_ASCII'] = False
